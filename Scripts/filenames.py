c33c629f28acc4781fab57ba2de7765b8560ea13
Gueni <gueni.mohamed@eniso.u-sousse.tn>
2023-06-29 14:08:28 +0100

    added auto move file feature with filters

--- Scripts/filenames.py
+++ Scripts/filenames.py
@@ -1,18 +1,21 @@
 
-import os
+import os,shutil
 
-def rename_files(folder_path):
+def rename_files(folder_path,destination_folder):
     file_list = os.listdir(folder_path)  # Get the list of file names in the folder
 
     for file_name in file_list:
-        old_file_path = os.path.join(folder_path, file_name)
-        new_file_name = str(hash(file_name)) + file_name[:-1]  # Add a unique number and remove the last character
+        if file_name.endswith(".mp4_"):
+            old_file_path = os.path.join(folder_path, file_name)
+            new_file_name = str(hash(file_name)) + file_name[:-1]  # Add a unique number and remove the last character
 
-        new_file_path = os.path.join(folder_path, new_file_name)
-        os.rename(old_file_path, new_file_path)  # Rename the file
-
+            new_file_path = os.path.join(folder_path, new_file_name)
+            os.rename(old_file_path, new_file_path)  # Rename the file
+            #move file to destination            
+            shutil.move(new_file_path, destination_folder)
     print("Files renamed successfully!")
 
 #---------------------------------------------------------------------
-folder_path = "C:/Users/HomePC/Videos"
-rename_files(folder_path)
+folder_path         = "C:/Users/HomePC/Downloads"
+destination_folder  = "D:/8 PC"
+rename_files(folder_path,destination_folder)
